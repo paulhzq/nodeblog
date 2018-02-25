eacffda9a2208f50f01b739e5089bf7630e58373
Paul Huang <paul_hzq@hotmail.com>
2018-02-24 21:02:34 -0500

    add the addposts page

---
+++ routes/posts.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req,res,next){
+  res.render('addpost',{
+    "title": "Add Post"
+  });
+});
+module.exports = router;
